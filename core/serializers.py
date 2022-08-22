56fa73b33052b147292986316e8257b978cfdfaa
Yasbernardi <yasmim.bernardii@gmail.com>
2022-08-22 14:26:15 -0300

    commit final

---
+++ core/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework.serializers import ModelSerializer
+
+from core.models import Categoria, Carro, Marca
+
+
+class CarroSerializer(ModelSerializer):
+    class Meta:
+        model = Carro
+        fields = "__all__"
+
+class CategoriaSerializer(ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = "__all__"
+
+class MarcaSerializer(ModelSerializer):
+    class Meta:
+        model = Marca
+        fields = "__all__"
